b873c9f050aa7e307561e9145fee0599d81a067b
NamNV609 <namnv609@gmail.com>
2016-04-07 23:39:58 +0700

    β version :grin:

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# chatwork-kick-out-notify
-ChatWork kick out notify extension for Google Chrome
+# How to install
+
+* Download file ``chatwork-kickout-notify.crx``
+* Open Google Chrome with address ``chrome://extensions``
+* Check to __Developer mode__
+* Drag and drop file ``chatwork-kickout-notify.crx`` to this page
+* Reload Google Chrome or webpage contain Flickr URLs
+* Right click to any image or link then click to ``Go to Flickr`` in menu context
---
+++ assets/js/cw.js
@@ -0,0 +1,76 @@
+$(window).ready(function() {
+    setTimeout(function() {
+        executeExtension();
+        $("#_roomListItems").bind("DOMSubtreeModified", function() {
+            executeExtension();
+        });
+    }, 1000);
+});
+
+executeExtension = function() {
+    var roomList = getRoomList();
+    var listKickedRoom = checkIsKickedOut(roomList);
+
+    if (listKickedRoom.length > 0) {
+        localStorage.CW_KICKOUT_NOTIFY_ROOM_LIST = getRoomList();
+        alert("You got to kick off [" + listKickedRoom.join(" / ") + "] box");
+    }
+}
+
+getRoomList = function() {
+    var roomList = [];
+
+    $("#_roomListItems li[role=listitem]").each(function() {
+        var roomId = $(this).attr("data-rid"),
+            roomName = $(this).attr("aria-label"),
+            roomInfo = {};
+        roomInfo[roomId] = roomName;
+
+        roomList.push(roomInfo);
+    });
+
+    return JSON.stringify(roomList);
+}
+
+checkIsKickedOut = function(currentRoomList) {
+    var oldRoomList = localStorage.CW_KICKOUT_NOTIFY_ROOM_LIST,
+        listKickedRoom = [];
+
+    if (oldRoomList) {
+        oldRoomList = JSON.parse(oldRoomList);
+        currentRoomList = JSON.parse(currentRoomList);
+        if (oldRoomList.length > currentRoomList.length) {
+            listKickedRoom = getKickedRoomName(oldRoomList, currentRoomList);
+        } else if (oldRoomList.length < currentRoomList.length) {
+            localStorage.CW_KICKOUT_NOTIFY_ROOM_LIST = JSON.stringify(currentRoomList);
+        }
+    } else {
+        localStorage.CW_KICKOUT_NOTIFY_ROOM_LIST = currentRoomList;
+    }
+
+    return listKickedRoom;
+}
+
+getKickedRoomName = function(oldList, currentList) {
+    var listRoom = [];
+    currentList = arrayObjectReduce(currentList);
+
+    for (var i = 0; i < oldList.length; i++) {
+        var roomId = Object.keys(oldList[i]).join();
+
+        if (typeof currentList[roomId] === "undefined") {
+            listRoom.push(oldList[i][roomId]);
+        }
+    }
+
+    return listRoom;
+}
+
+arrayObjectReduce = function(arrObj) {
+    return arrObj.reduce(function(result, item) {
+        var key = Object.keys(item)[0];
+        result[key] = item[key];
+
+        return result;
+    }, {});
+}
